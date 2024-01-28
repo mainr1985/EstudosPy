0f1f358e15d0f0ffb6a6b570f8ac1a25263b2021
Maíra N. Rocha <mairanrocha@hotmail.com>
2024-01-27 22:10:43 -0300

    Primeiro commit estudos Python 3

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+# hello world e estudo de definição de variáveis
+
+frase = 'Hello World Python VS Code'
+frase2 = "Estudando Python"
+ano = 2024
+#27nome = input ('Digite seu nome')
+idade = int(input('Digite a sua idade: ')) #faz a conversão da string digitada pra int
+soma = idade + 1
+
+#print (frase)
+#print (frase2)
+#print (frase2 + " " + "para concursos em", ano)
+#print ("Olá estudante, já sei que seu nome é " + nome)
+#print ("Sua idade é ", idade)
+print (soma)
+
