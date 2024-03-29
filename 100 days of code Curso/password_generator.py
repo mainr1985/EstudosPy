16c84230175bb800460116bde2d9311a3d87f9e6
Maíra N. Rocha <mairanrocha@hotmail.com>
2024-03-29 13:21:38 -0300

    Password Generator in Python

---
+++ 100 days of code Curso/password_generator.py
@@ -0,0 +1,28 @@
+import random
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
+
+numbers = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print ("Welcome to the password generator")
+num_letters = int(input("How many letters would you like to have in your password?\n"))
+num_numbers = int(input("How many numbers would you like to have in your password?\n"))
+num_symbols = int(input("How many symbols would you like to have in your password?\n"))
+
+random_password = []
+for n in range (1,num_letters+1):
+    random_password.append(random.choice (letters)) #percorre todo o array selecionado, escolhe aleatoriamente e coloca no novo array password
+        
+for n in range (1, num_numbers+1):
+    random_password.append(random.choice (numbers))
+    
+for n in range (1, num_symbols+1):
+    random_password.append(random.choice (symbols))
+    
+random.shuffle(random_password)
+password = ""
+for randpass in random_password:
+    password += randpass #concatenação
+
+print(f"This is your new suggested password:\n {password}")
