2972afe1b2cc8c42a7efa16740ec77650c5303ea
Maíra N. Rocha <mairanrocha@hotmail.com>
2024-03-29 13:26:45 -0300

    Gerador de Senhas Python

---
+++ 100 days of code Curso/gerador_senha.py
@@ -0,0 +1,28 @@
+import random
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+           'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z']
+
+numbers = ['0','1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '@', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print ("Bem vindo(a) ao gerador de senhas Python")
+num_letters = int(input("Quantas letras você quer na sua senha?\n"))
+num_numbers = int(input("Quantos números você quer na sua senha?\n"))
+num_symbols = int(input("Quantos símbolos você quer na sua senha?\n"))
+
+random_password = []
+for n in range (1,num_letters+1):
+    random_password.append(random.choice (letters)) 
+        
+for n in range (1, num_numbers+1):
+    random_password.append(random.choice (numbers))
+    
+for n in range (1, num_symbols+1):
+    random_password.append(random.choice (symbols))
+    
+random.shuffle(random_password)
+password = ""
+for randpass in random_password:
+    password += randpass
+
+print(f"Essa é a sua sugestão de senha:\n {password}")
